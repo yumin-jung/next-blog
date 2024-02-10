ab45ebc7065b6eca2ac184beac232b0df548e4ab
yumin-jung <jungym887@naver.com>
2024-02-11 01:12:59 +0900

    design: Update blog home design with grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Link from "next/link"
 
 const Home = ({ allPostsData }: {
   allPostsData: {
-    date: string
     title: string
     id: string
   }[]
@@ -17,23 +16,17 @@ const Home = ({ allPostsData }: {
         <Head>
           <title>Yumin</title>
         </Head>
-        <section className={homeStyles.headingMd}>
-          <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
-            <h2 className={homeStyles.headingLg}>기록 남기기</h2>
-            <ul className={homeStyles.list}>
-              {allPostsData.map(({ id, title, date }) =>
-                <li className={homeStyles.listItem} key={id}>
-                  <Link href={`/posts/${[id]}`}>
-                    {title}
-                  </Link>
-                  <br />
-                  <small className={homeStyles.lightText}>
-                    {date}
-                  </small>
-                </li>
-              )}
-            </ul>
-          </section>
+        <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+          <h2 className={homeStyles.headingLg}>기록 남기기</h2>
+          <ul className={homeStyles.grid}>
+            {allPostsData.map(({ id, title }) =>
+              <div key={id}>
+                <Link href={`/posts/${[id]}`} style={{ textDecoration: 'none' }}>
+                  <div className={homeStyles.gridItem}>{title}</div>
+                </Link>
+              </div>
+            )}
+          </ul>
         </section>
       </div >
     </>
