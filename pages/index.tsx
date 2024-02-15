f42a3daf3ddca1c15e39eb180386cb31c8be13d2
yumin-jung <jungym887@naver.com>
2024-02-15 22:57:56 +0900

    design: Add grid style in `index.tsx` and apply wiggle animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,15 @@ const Home = ({ allPostsData }: {
         </Head>
         <section className={`text-[1.2rem] leading-normal pt-px`}>
           <h2 className={`text-2xl leading-[1.4] mx-0 my-4`}>기록 남기기 📝</h2>
-          <ul className={`flex flex-col gap-4 pt-6`}>
+          <ul className={`grid grid-cols-[repeat(3,1fr)] gap-4 pt-6`}>
             {allPostsData.map(({ id, title }) =>
               <Link
                 key={id}
                 href={`/posts/${[id]}`}
                 style={{ textDecoration: 'none' }}
+                className={`text-base aspect-[1.2] hover:animate-wiggle
+                flex flex-col items-center shadow-[rgba(0,0,0,0.1)_0px_4px_12px] 
+                p-6 rounded-lg`}
               >
                 {title}
               </Link>
