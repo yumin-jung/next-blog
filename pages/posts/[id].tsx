1c2c1c5416be3a6c1ff2576549457dd9d4be9ca2
yumin-jung <jungym887@naver.com>
2024-02-20 13:59:57 +0900

    refactor: Refactor function related with posts
    formatDate, formatHtml

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -10,6 +10,32 @@ const variants = {
   hidden: { opacity: 0 },
 }
 
+/**
+   * yyyy-mm-dd 형태의 데이터를 년, 월, 일 형태로 변환한다.
+   * @param {string} inputDate - yyyy-mm-dd 형태의 날짜 형식 문자열
+   * @returns {string}
+   */
+function formatDate(inputDate: string): string {
+  const date = new Date(inputDate)
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return year + "년 " + month + "월 " + day + "일"
+}
+
+/**
+ * code 태그 내부를 제외한 newline을 <br/>로 치환한다.
+ * @param {string} inputHtml - html 형식의 문자열
+ * @returns {string}
+ */
+function formatHtml(inputHtml: string): string {
+  const regex = /<code[\s\S]*?<\/code>|(\n)/g
+  return inputHtml.replace(regex, (match, group) => {
+    return group ? "<br/>" : match
+  })
+}
+
 const Post = ({
   postData,
 }: {
@@ -19,28 +45,6 @@ const Post = ({
     contentHtml: string
   }
 }) => {
-  /* code tag 안에서는 newline이 <br/>이 되지 않고 나머지 경우만 적용 */
-  const regex = /<code[\s\S]*?<\/code>|(\n)/g
-  const html = postData.contentHtml.replace(regex, (match, group) => {
-    return group ? "<br/>" : match
-  })
-
-  /**
-   * yyyy-mm-dd 형태의 데이터를 년, 월, 일 형태로 변환한다.
-   * @param {string} inputDate - yyyy-mm-dd 형태의 날짜 형식 문자열
-   * @returns {string}
-   */
-  const formatDate = (inputDate: string): string => {
-    const date = new Date(inputDate)
-    const year = date.getFullYear()
-    const month = date.getMonth() + 1
-    const day = date.getDate()
-
-    return year + "년 " + month + "월 " + day + "일"
-  }
-
-  const postDate = formatDate(postData.date)
-
   return (
     <motion.div
       className="max-w-xl mt-16 mb-24 mx-auto px-4 py-0"
@@ -53,10 +57,10 @@ const Post = ({
       </Head>
       <article className="mb-12">
         <h1 className="text-[1.6rem] leading-[1.4] mt-8">{postData.title}</h1>
-        <div className="text-[0.9rem] mt-4 mb-8">{postDate}</div>
+        <div className="text-[0.9rem] mt-4 mb-8">{formatDate(postData.date)}</div>
         <div
           className="text-[1.1rem] leading-[1.8] font-light"
-          dangerouslySetInnerHTML={{ __html: html }}
+          dangerouslySetInnerHTML={{ __html: formatHtml(postData.contentHtml) }}
         />
       </article>
       <Comment />
