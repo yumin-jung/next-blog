c8c16f1602a70f9195f164c0ec6d3538c5e7cb06
yumin-jung <jungym887@naver.com>
2024-02-24 14:03:35 +0900

    fix: Fix FOUT in `index.tsx`
    Remove Noto_Sans_KR font in `layout.tsx`

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,8 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Noto_Sans_KR } from "next/font/google"
-
-const notoSansKr = Noto_Sans_KR({
-  weight: ["300"],
-  subsets: [],
-})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={notoSansKr.className}>
+    <div>
       {children}
       <SpeedInsights />
     </div>
