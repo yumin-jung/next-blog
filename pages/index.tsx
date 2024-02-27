3e9b77f40bd19a2998a25d8c7b32367920181b9b
yumin-jung <jungym887@naver.com>
2024-02-28 01:37:56 +0900

    design: Update animation in `index.tsx`

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ const container = {
     opacity: 1,
     scale: 1,
     transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
+      delayChildren: 0.1,
+      staggerChildren: 0.1,
     },
   },
 }
