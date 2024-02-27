8888646557fec41c0495f72fb35391a4a5fd0b9a
yumin-jung <jungym887@naver.com>
2024-02-28 01:05:45 +0900

    refactor: Refactor code for font optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,39 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import Layout from "@/components/layout"
+import localFont from "next/font/local"
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "../public/fonts/PretendardVariable.woff2",
+      weight: "200",
+    },
+  ],
+})
+
+const tossface = localFont({
+  src: [
+    {
+      path: "../public/fonts/TossFaceFontWeb.otf",
+      weight: "400",
+    },
+  ],
+})
+
+const sourcecodepro = localFont({ src: "../public/fonts/SourceCodePro-Regular.otf" })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <style jsx global>{`
+        :root {
+          --pretendard-font: ${pretendard.style.fontFamily};
+          --tossface-font: ${tossface.style.fontFamily};
+          --sourcecodepro-font: ${sourcecodepro.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
     </Layout>
   )
 }
